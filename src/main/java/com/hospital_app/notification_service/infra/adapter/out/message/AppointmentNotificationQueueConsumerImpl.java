2288269d7e09dc8481ef7b288f59bd1bb9dfdb61
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-08-31 01:50:38 -0300

    feat: Consuming appointment message

--- src/main/java/com/hospital_app/notification_service/infra/adapter/out/message/AppointmentNotificationQueueConsumerImpl.java
+++ src/main/java/com/hospital_app/notification_service/infra/adapter/out/message/AppointmentNotificationQueueConsumerImpl.java
@@ -12,5 +12,11 @@ public class AppointmentNotificationQueueConsumerImpl implements AppointmentNoti
     @Override
     public void consume(Appointment appointment) {
         System.out.println("Appointment received: " + appointment);
+        System.out.println("Appointment id: " + appointment.getId());
+        System.out.println("Appointment doctor id: " + appointment.getDoctorId());
+        System.out.println("Appointment patient id: " + appointment.getPatientId());
+        System.out.println("Appointment status: " + appointment.getStatus());
+        System.out.println("Appointment notes: " + appointment.getNotes());
+        System.out.println("Appointment date time: " + appointment.getDateTime());
     }
 }
